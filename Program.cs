a4e1ba6babef7a40169a660b7b9bf51c05dd2ac6
Lucas <lucas.souza3146@gmail.com>
2022-12-14 17:57:08 -0300

    atualizado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -6,27 +7,40 @@ var app = builder.Build();
 app.MapPost("/products", (Product product) =>
 {
     ProductRepository.Add(product);
+    return Results.Created("/products/" + product.Code, product.Code);
 });
 
 app.MapGet("/products/{code}", ([FromRoute] string code) =>
 {
     var product = ProductRepository.GetBy(code);
-    return product;
+    if (product != null)
+        return Results.Ok(product);
+    return Results.NotFound();
 });
 
 app.MapPut("/products", (Product product) =>
 {
     var productSaved = ProductRepository.GetBy(product.Code);
     productSaved.Name = product.Name;
-    return "Atualizado com sucesso!";
+    return Results.Ok();
 });
 
 app.MapDelete("/products/{code}", ([FromRoute] string code) =>
 {
     var productSaved = ProductRepository.GetBy(code);
     ProductRepository.Remove(productSaved);
-    return "Removido com sucesso!";
+    return Results.Ok();
 });
+
+app.MapGet("/configuration/database", (IConfiguration configuration) =>
+{
+    return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+});
+
+ProductRepository.Add(new Product { Code = "1", Name = "HDMI"});
+ProductRepository.Add(new Product { Code = "2", Name = "USB"});
+ProductRepository.Add(new Product { Code = "3", Name = "Smart TV"});
+ProductRepository.Add(new Product { Code = "4", Name = "Notebook dell"});
 
 app.Run();
 
@@ -36,11 +50,6 @@ public static class ProductRepository
 
     public static void Add(Product product)
     {
-        if (product == null)
-	    {
-            product = new Product();
-	    }
-
         Products.Add(product);
     }
 
